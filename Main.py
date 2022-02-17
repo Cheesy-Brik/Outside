645472a38722fe79e38101c6cf2f143c269ea599
Cheesy-Brik <loweryan168@yahoo.com>
2022-02-16 22:48:15 -0600

    "Automatic File Updates"

--- Main.py
+++ Main.py
@@ -378,7 +378,7 @@ def fetch_square(id = 0, x = 0, y = 0, zoom = 1000):#Extremely messy code ---V
     if 'crude wooden wall' in placements:vis='🌰'
     if 'crude furnace' in placements:vis='🪔'
     
-    if changed: 
+    if str(pos) in save['terrrain']['overide']:
         if 'dropped_items' in changed:vis='🧺' if changed['dropped_items'] else ''
     
     player = False
