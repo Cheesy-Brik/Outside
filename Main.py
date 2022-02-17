d6188b2445a33a06a8a18e954eee1b753ee0841f
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-02-17 19:37:58 +0800

    e

--- Main.py
+++ Main.py
@@ -1010,12 +1010,9 @@ async def help(ctx, x=0, y=0, zoom = 1000, size =10):
 
     embed = discord.Embed(title='Help', description='*Command prefix is* ``!``', color=0x00ff00)
 
-    try:
-        embed.set_thumbnail(url=ctx.me.avatar.url)
-    except Exception as e:
-        user = await client.fetch_user(807757190316163104)
-        embed.set_thumbnail(url=user.avatar.url)
-        print(e)
+    user = await client.fetch_user(807757190316163104)
+    embed.set_thumbnail(url=user.avatar.url)
+    
     for i in client.commands:
          if i.help:embed.add_field(name = f'-**{str(i.name)}**- ' + ('('+ ', '.join(aliase for aliase in i.aliases) +')') if i.aliases else '', value=i.help,inline=False)#command objects are genrators so you have to parse to str
     await ctx.reply(embed=embed)
