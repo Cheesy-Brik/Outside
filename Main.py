7bc4d461edd2d4a12dbf748d300015043bad4f19
Cheesy-Brik <loweryan168@yahoo.com>
2022-02-21 21:07:01 -0600

    "Automatic File Updates"

--- Main.py
+++ Main.py
@@ -1504,7 +1504,11 @@ async def forcerespawn(ctx):
         await ctx.reply('To confirm this @ yourself when using the command, also this respawn does have all the normal effects of a normal respawn')
         return
     if ctx.message.mentions[0].id == ctx.author.id:
-        respawn(ctx.author.id)
+        dropped = random.choice(list(save['users'][id]['inv'].keys()))
+        await drop(ctx, amount=save['users'][id]['inv'][dropped]['amount']//2,item=dropped)
+        await ctx.reply('You took too much damage and you died')
+        respawn(id)
+        return
     else:
         await ctx.reply('To confirm this @ yourself when using the command, also this respawn does have all the normal effects of a normal respawn')
         return
