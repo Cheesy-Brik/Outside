05b09792c40acdd0cab91a8025c5e68d1fc20cbc
Cheesy-Brik <loweryan168@yahoo.com>
2022-03-01 20:10:29 -0600

    "Automatic File Updates"

--- Main.py
+++ Main.py
@@ -651,8 +651,7 @@ def fetch_square(id = 0, x = 0, y = 0, zoom = 1000):#Extremely messy code ---V
     for i in save['terrain']['nations']:#Where nations is a dict
             for j in save['terrain']['nations'][i]['claims']:#Where j is a tuple
                 claim_x, claim_y = tuple(j);claim_x, claim_y = (claim_x/1000, claim_y/1000)#           :)
-                print(pos[0], pos[1], claim_x+4, claim_y+4)
-                if (pos[0]<claim_x+4 and pos[0]>=claim_x) and (pos[1]<claim_y+4 and pos[1]>=claim_y):
+                if (pos[0]<claim_x+4/1000 and pos[0]>=claim_x) and (pos[1]<claim_y+4/1000 and pos[1]>=claim_y):
                     nation = i
                     break
             else:continue
